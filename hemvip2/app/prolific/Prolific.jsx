ea59acaaa0a037f1d809173d9e75c05e9d32d53c
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2024-06-25 17:39:03 +0700

    update prolific

--- hemvip2/app/prolific/Prolific.jsx
+++ hemvip2/app/prolific/Prolific.jsx
@@ -1,7 +1,78 @@
-import React from 'react'
+"use client"
 
-export default function Prolific() {
-  return (
-    <div>Prolific</div>
-  )
+import React from 'react'
+import axios from "axios"
+import { Screen } from "@/components/screen"
+import { ExperimentConfigProvider } from "@/contexts/experiment"
+import { ScreenControlProvider } from "@/contexts/screencontroll"
+import { ActionRecorderProvider } from "@/contexts/action-recorder"
+import PreventRefreshPage from "@/components/PreventRefreshPage"
+import { Callout } from "@/components/core"
+import { StudyProvider } from "@/contexts/study"
+import { Suspense, useEffect, useState } from "react"
+import { API_ENDPOINT } from "@/utils/urlEndpoint"
+import LoadingSpin from "@/components/loading/LoadingSpin"
+import { UnloadProvider } from "@/contexts/beforeunload"
+import { useSearchParams } from 'next/navigation'
+
+export function Prolific() {
+    const searchParams = useSearchParams();
+    const PROLIFIC_PID = searchParams.get('PROLIFIC_PID');
+    const STUDY_ID = searchParams.get('STUDY_ID');
+    const SESSION_ID = searchParams.get('SESSION_ID');
+
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [isSuccess, setIsSuccess] = useState(false);
+    const [error, setError] = useState(null);
+
+
+    useEffect(() => {
+        const fetchStudies = async () => {
+            const response = await axios.get(`${API_ENDPOINT}/api/studies?prolificid=${PROLIFIC_PID}&studyid=${STUDY_ID}&sessionid=${SESSION_ID}`)
+
+            const { errors, success, data, msg } = response.data
+            console.log("msg", msg)
+            setError(errors)
+            setIsSuccess(success)
+            setData(data)
+        }
+        setLoading(true)
+        fetchStudies()
+        setLoading(false)
+    }, [PROLIFIC_PID, STUDY_ID, SESSION_ID])
+
+    if (loading) {
+        return (
+            <div className="w-full max-h-screen h-screen bg-gray-100 overflow-hidden">
+                <LoadingSpin />
+            </div>
+        )
+    }
+
+    if (!data || data.length <= 0) {
+        return (
+            <div className="w-full max-h-screen h-screen bg-gray-100 overflow-hidden">
+                <LoadingSpin />
+            </div>
+        )
+    }
+
+    return (
+        <ExperimentConfigProvider value={data}>
+            <ScreenControlProvider min={0} max={data.pages.length - 1}>
+                <ActionRecorderProvider pages={data.pages}>
+                    <StudyProvider>
+                        <UnloadProvider>
+                            <Screen
+                                prolificid={PROLIFIC_PID}
+                                studyid={STUDY_ID}
+                                sessionid={SESSION_ID}
+                            />
+                        </UnloadProvider>
+                    </StudyProvider>
+                </ActionRecorderProvider>
+            </ScreenControlProvider>
+        </ExperimentConfigProvider>
+    )
 }
--- hemvip2/app/prolific/page.jsx
+++ hemvip2/app/prolific/page.jsx
@@ -13,49 +13,12 @@ import { Suspense, useEffect, useState } from "react"
 import LoadingSpin from "@/components/loading/LoadingSpin"
 // import { UnloadProvider } from "@/contexts/beforeunload"
 import { useSearchParams } from 'next/navigation'
+import { Prolific } from "./Prolific";
 
 export default function Page() {
-  // const searchParams = useSearchParams();
-  // const PROLIFIC_PID = searchParams.get('PROLIFIC_PID');
-  // const STUDY_ID = searchParams.get('STUDY_ID');
-  // const SESSION_ID = searchParams.get('SESSION_ID');
-
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [isSuccess, setIsSuccess] = useState(false);
-  const [error, setError] = useState(null);
 
 
-  // useEffect(() => {
-  //   const fetchStudies = async () => {
-  //     const response = await axios.get(`${API_ENDPOINT}/api/studies?prolificid=${PROLIFIC_PID}&studyid=${STUDY_ID}&sessionid=${SESSION_ID}`)
-
-  //     const { errors, success, data, msg } = response.data
-  //     console.log("msg", msg)
-  //     setError(errors)
-  //     setIsSuccess(success)
-  //     setData(data)
-  //   }
-  //   setLoading(true)
-  //   fetchStudies()
-  //   setLoading(false)
-  // }, [PROLIFIC_PID, STUDY_ID, SESSION_ID])
-
-  if (loading) {
-    return (
-      <div className="w-full max-h-screen h-screen bg-gray-100 overflow-hidden">
-        <LoadingSpin />
-      </div>
-    )
-  }
-
-  if (!data || data.length <= 0) {
-    return (
-      <div className="w-full max-h-screen h-screen bg-gray-100 overflow-hidden">
-        <LoadingSpin />
-      </div>
-    )
-  }
+  
 
   // if (!isSuccess || !data) {
   //   return (
@@ -81,22 +44,8 @@ export default function Page() {
 
   return (
     <Suspense fallback={<div>loading...</div>}>
-      <div className="w-full max-h-screen h-screen bg-gray-100 overflow-hidden">34
-        {/* <ExperimentConfigProvider value={data}>
-          <ScreenControlProvider min={0} max={data.pages.length - 1}>
-            <ActionRecorderProvider pages={data.pages}>
-              <StudyProvider>
-                <UnloadProvider>
-                  <Screen
-                    prolificid={PROLIFIC_PID}
-                    studyid={STUDY_ID}
-                    sessionid={SESSION_ID}
-                  />
-                </UnloadProvider>
-              </StudyProvider>
-            </ActionRecorderProvider>
-          </ScreenControlProvider>
-        </ExperimentConfigProvider> */}
+      <div className="w-full max-h-screen h-screen bg-gray-100 overflow-hidden">
+        <Prolific />
       </div>
       {/* <PreventRefreshPage /> */}
       {/* <PaginationScreen /> */}
