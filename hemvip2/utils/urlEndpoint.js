97ef03bc3636c5476d5f0c4e670d32efa98f3bb4
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2024-07-03 23:10:59 +0700

    update env sample

--- hemvip2/utils/urlEndpoint.js
+++ hemvip2/utils/urlEndpoint.js
@@ -2,4 +2,4 @@
 
 export const API_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT || "https://api.hemvip.workers.dev"
 
-export const HEMSPEED_WEBSOCKET = process.env.NEXT_PUBLIC_HEMSPEED_WEBSOCKET || "https://hemspeed.hemvip.workers.dev"
+export const HEMSPEED_WEBSOCKET = process.env.NEXT_PUBLIC_HEMSPEED_WEBSOCKET || "wss://hemspeed.hemvip.workers.dev"
