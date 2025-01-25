6800e7ee89acd7a11f1df3093250e097b29a4429
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-01-26 05:20:24 +0700

    update study

--- hemvip2/contexts/study.js
+++ hemvip2/contexts/study.js
@@ -4,30 +4,26 @@ import { DEFAULT_OPTION } from "@/config/constants"
 import { createContext, useContext, useState } from "react"
 
 const StudyContext = createContext({
-  options: {
-    0: DEFAULT_OPTION.unselect,
-    1: DEFAULT_OPTION.unselect,
-  }, // sample, this will be replace
-  selectStudy: (newOption, currentPage) => {},
+	options: {
+		0: DEFAULT_OPTION.unselect,
+		1: DEFAULT_OPTION.unselect,
+	}, // sample, this will be replace
+	selectStudy: (newOption, currentPage) => {},
 })
 
 export const useStudy = () => useContext(StudyContext)
 export function StudyProvider({ children }) {
-  const [options, setOptions] = useState({})
+	const [options, setOptions] = useState({})
 
-  const selectStudy = (newOption, currentPage) => {
-    console.log(newOption, currentPage)
-    setOptions(() => {
-      options[currentPage] = newOption
+	const selectStudy = (newOption, currentPage) => {
+		console.log(newOption, currentPage)
+		setOptions(() => {
+			options[currentPage] = newOption
 
-      return options
-    })
-  }
+			return options
+		})
+	}
 
-  // actions list sample will be replace by [] here
-  return (
-    <StudyContext.Provider value={{ options, selectStudy }}>
-      {children}
-    </StudyContext.Provider>
-  )
+	// actions list sample will be replace by [] here
+	return <StudyContext.Provider value={{ options, selectStudy }}>{children}</StudyContext.Provider>
 }
