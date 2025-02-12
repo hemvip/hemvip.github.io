457b7c278a966d4f4eb4fb43fd2e438904a199db
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-02-12 16:56:58 +0700

    startup guide

---
+++ hemvip2/app/StartupGuide.jsx
@@ -0,0 +1,31 @@
+export default function StartupGuide() {
+	return (
+		<>
+			<h2 className="font-semibold tracking-tight text-slate-900 dark:text-slate-100 mt-3 text-2xl">Startup guide</h2>
+			<h3 className="font-semibold tracking-tight text-slate-900 dark:text-slate-100 mt-3 text-xl">1. Our Gesture Generation Study</h3>
+			<p className="mt-3 leading-6 first:mt-0">
+				Gesture Generation is the process of generating gestures from speech or text. The goal of Gesture Generation is to generate gestures that are natural, realistic, and appropriate for the given
+				context. The generated gestures can be used to animate virtual characters, robots, or embodied conversational agents.
+			</p>
+			<p className="mt-3 leading-6 first:mt-0">Guide</p>
+			<ul className="mt-3 list-disc first:mt-0 ltr:ml-6 rtl:mr-6">
+				<li className="task-list-item">Exist screen when paritipate</li>
+				<li className="task-list-item">Update the website...</li>
+				<li className="task-list-item">Contact the media...</li>
+			</ul>
+			<h3 className="font-semibold tracking-tight text-slate-900 dark:text-slate-100 mt-3 text-xl">2. Process of Gesture Generation Study</h3>
+			<p className="mt-3 leading-6 first:mt-0">
+				Start our study by click{" "}
+				<code className="nextra-code" dir="ltr">
+					Start
+				</code>
+			</p>
+			<p className="mt-6 leading-6 first:mt-0">
+				Please choose{" "}
+				<code className="nextra-code" dir="ltr">
+					Next
+				</code>
+			</p>
+		</>
+	)
+}
--- hemvip2/app/page.jsx
+++ hemvip2/app/page.jsx
@@ -1,7 +1,5 @@
 "use client"
 
-import { Suspense, useState } from "react"
-import LoadingSpin from "@/components/loading/LoadingSpin"
 import { HomePage } from "./HomePage"
 import { useSearchParams } from "next/navigation"
 // import AttentionCheck from "@/components/attentioncheck/AttentionCheck"
@@ -9,29 +7,30 @@ import { useSearchParams } from "next/navigation"
 export default function Home() {
 	// const [isOpenDialog, setIsOpenDialog] = useState(false)
 	// const [state, setState] = useState("Attention Check")
-	const [state, setState] = useState("Start Study")
-	const [loading, setLoading] = useState(false)
-
+	// const [state, setState] = useState("Start Study")
 	const searchParams = useSearchParams()
 	const prolificid = searchParams.get("PROLIFIC_PID")
 	const studyid = searchParams.get("STUDY_ID")
 	const sessionid = searchParams.get("SESSION_ID")
 
 	return (
-		<Suspense fallback={<div></div>}>
-			<main className="flex w-full flex-col items-center justify-between p-2 md:px-24">
-				<HomePage />
-				{/* <HomePage handleAttentionCheck={handleAttentionCheck} state={state} /> */}
-				{/* <AttentionCheck
+		<main className="flex w-full flex-col items-center justify-between p-2 md:px-24">
+			<div className="w-full px-[7%] gap-2 py-2 flex flex-col">
+				<div className="flex flex-col w-full h-full gap-2">
+					<div className="lg:px-8 px-0 overflow-y-auto">
+						<h1 className="mt-2 text-2xl sm:text-4xl font-bold tracking-tight text-slate-900 dark:text-slate-100">Pairwise Comparison of Gesture Generation AI Model Studies</h1>
+
+						<HomePage prolificid={prolificid} studyid={studyid} sessionid={sessionid} />
+						{/* <AttentionCheck
           isOpenDialog={isOpenDialog}
           setIsOpenDialog={setIsOpenDialog}
           setState={setState}
           setLoadingAttentionCheck={setLoading}
         /> */}
-				{loading && <LoadingSpin />}
-			</main>
-		</Suspense>
+						{/* {loading && <LoadingSpin />} */}
+					</div>
+				</div>
+			</div>
+		</main>
 	)
 }
-
-// export const runtime = 'edge';
