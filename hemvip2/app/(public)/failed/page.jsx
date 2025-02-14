cd0fab456bf296e45b24f06100783da8475dce66
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-02-14 18:50:47 +0700

    add failed page

---
+++ hemvip2/app/(public)/failed/page.jsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { Callout } from "@/components/core"
+import { Suspense } from "react"
+
+export default function Page() {
+	return (
+		<Suspense fallback={<div></div>}>
+			<div className="w-full max-h-screen h-screen bg-gray-100 overflow-hidden">
+				<div className="w-full max-h-screen h-screen bg-gray-100 overflow-hidden">
+					<div className="h-screen inset-0 flex items-center justify-center z-50">
+						<Callout type="warning" className="min-w-96">
+							<div className="flex gap-2 flex-col ">
+								<h2 className="text-xl font-semibold">Study Failed</h2>
+								<p className="leading-7 first:mt-0">Your study is uncomplete because of failed attention check</p>
+								<div className="">
+									Visit{" "}
+									<a
+										className="text-primary-600 underline decoration-from-font [text-underline-position:from-font]"
+										href="https://www.prolific.com/"
+									>
+										Prolific
+									</a>{" "}
+									to start new study.
+								</div>
+							</div>
+						</Callout>
+					</div>
+				</div>
+			</div>
+		</Suspense>
+	)
+}
