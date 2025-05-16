533169e55c74137e8a65b0b0417f8443abe2a23d
Rajmund Nagy <rajmund977@gmail.com>
2025-05-16 04:25:01 +0200

    Add info about next button

--- hemvip2/components/screen/StartupScreen.jsx
+++ hemvip2/components/screen/StartupScreen.jsx
@@ -70,6 +70,7 @@ const StartupScreen = memo(function StartupScreen() {
 						<br/>
 						<ul className="list-disc list-inside">
 							<li>⚠️ Make sure to watch each video all the way through.</li>
+							<li>⚠️ The Next button is in the top right corner of the screen.</li>
 							<li>⚠️ You will encounter attention check questions. Please follow their instructions carefully — failing them may disqualify you and you will not be paid.</li>
 							<li>⚠️ Only use the “Skip screen” button if there is a technical issue (like a video not loading). Do not refresh the page — this will restart the experiment.</li>
 							<li>⚠️ If you have any questions, please contact us by sending a message on Prolific!</li>
