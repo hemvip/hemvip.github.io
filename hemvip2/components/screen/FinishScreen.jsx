c421371f94e0f3679b30afb209241be75f7d7814
Rajmund Nagy <rajmund977@gmail.com>
2025-05-16 07:57:09 +0200

    Remove finish screen

--- hemvip2/components/screen/FinishScreen.jsx
+++ hemvip2/components/screen/FinishScreen.jsx
@@ -15,43 +15,11 @@ export default function FinishScreen({ handleFinish, loadingFinish }) {
 	return (
 		<div className="px-8 overflow-y-auto">
 			<div className="mx-[10%]">
-				<h2 className="text-center font-semibold tracking-tight text-slate-900 dark:text-slate-100 text-2xl">
-					🏁 Your Selection Result 🏁
-				</h2>
 				<h3 className="font-semibold tracking-tight text-slate-900 dark:text-slate-100 mt-3 text-xl">
 					Submit your result to finish
 				</h3>
 
-				<p className="mt-3 leading-6 first:mt-0 text-sm">
-					You cannot edit on selected option here. To make changes, please click the &apos;Previous&apos; button to
-					reselect your option.
-				</p>
-				<div className={cn("-mx-6 mb-4 overflow-x-auto overscroll-x-contain px-6 pb-4", "mask-gradient")}>
-					<div className="w-full border-collapse text-base block">
-						<div className="block">
-							<div className="w-full px-4 flex border-b pt-2 text-left dark:border-neutral-700">
-								<div className="py-2 min-w-14 text-center font-semibold">#Page</div>
-								<div className="flex-grow text-center py-2 font-semibold">Your Selection</div>
-							</div>
-						</div>
-						<div className="flex flex-col align-baseline text-gray-900 dark:text-gray-100 h-80 overflow-y-auto">
-							{Object.entries(options).map(([pageid, selected], index) => {
-								return (
-									<div
-										className="flex w-full border-b border-t border-t-white border-gray-100 text-center dark:border-neutral-700/50 px-4"
-										key={index}
-									>
-										<div className="py-2 min-w-14 items-center flex justify-center text-center">{index + 1}</div>
-										{/* <div className=' py-2 '>{JSON.stringify(pages[pageid].content)}</div> */}
-										<div className="py-2 flex-grow">
-											<ScreenSelectedResult selected={selected} pageid={pageid} />
-										</div>
-									</div>
-								)
-							})}
-						</div>
-					</div>
-				</div>
+				
 				<h3 className="font-semibold tracking-tight text-slate-900 dark:text-slate-100 mt-3 text-xl"></h3>
 				{/* <p className="mt-3 leading-6 first:mt-0">
 					Gesture Generation is the process of generating gestures from speech or text. The goal of Gesture Generation is to generate gestures that are natural, realistic, and appropriate for the
